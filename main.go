6a0944d27328b62021ef36073e138631e3e90256
cohekoma <dinhhoangbaochan@gmail.com>
2026-02-15 22:48:22 +0700

    handle not found csv file and fail parsing, write func to quit app

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	csvFileName := flag.String("csv", "problems.csv", "default csv file for quizzes, format: question,answer")
+	csvFileName := flag.String("csv", "quizzes.csv", "default csv file for quizzes, format: question,answer")
 	flag.Parse()
-	fmt.Println(*csvFileName)
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		quitApp(fmt.Sprintf("Failed to open CSV file: %s. File may not exist!", *csvFileName))
+	}
+
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
+
+	if err != nil {
+		quitApp("Failed to parse the CSV file! Try using another file")
+	}
+
+	fmt.Println(lines)
+}
+
+func quitApp(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
